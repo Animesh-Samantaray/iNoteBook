de6567b00d114f54d91b6c28b7228d70489d6a90
Animesh Samantaray <your_email@github.com>
2025-08-09 21:20:53 +0530

    Changed to many

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1:27017/notebook';
-
-const connectDB = ()=>{
-        mongoose.connect(url).then((res)=>console.log('connected to db')).catch((err)=>console.log(err));
-}
-
-module.exports=  connectDB;
